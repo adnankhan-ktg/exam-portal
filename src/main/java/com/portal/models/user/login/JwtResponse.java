1eaa9ee16606f639b3fc256be5d532e28729b3a9
kuldeep.panwar007 <kuldeeppanwar7697@gmail.com>
2021-07-24 20:29:34 +0530

    correct spelling of active user

--- src/main/java/com/portal/models/user/login/JwtResponse.java
+++ src/main/java/com/portal/models/user/login/JwtResponse.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class JwtResponse {
 	private String token;
-	private String activeFistName;
+	private String activeFirstName;
 	private String  activeLastName;
 	private String role;
 
