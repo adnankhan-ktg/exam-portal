0dc51c2d8fdc4a2222d65ef107cc3600dcaac775
adnan khan <aka034437@gmail.com>
2021-08-01 15:09:54 +0530

    Get all user's dashboard data API created

--- src/main/java/com/portal/service/user/UserService.java
+++ src/main/java/com/portal/service/user/UserService.java
@@ -1,5 +1,7 @@
 package com.portal.service.user;
 
+import java.util.List;
+
 import com.portal.models.user.User;
 import com.portal.models.user.UserDashBoard;
 
@@ -8,5 +10,5 @@ public interface UserService {
 	public void deleteUser(User user);
 	public User updateUser(User user);
 	public UserDashBoard saveDashboard(UserDashBoard userDashBoard);
-
+	public List<UserDashBoard> getDashBoards();
 }
